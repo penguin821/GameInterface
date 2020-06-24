73e51ea140ed15f62b8dec5ce51e387028a40f68
penguin821 <penguin821@naver.com>
2020-06-24 22:31:20 +0900

    겜인페최종텀프

---
+++ CaveWithNoNameV2/Assets/Scripts/CheckTiles.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTiles : MonoBehaviour
+{
+    public SpriteRenderer checkTile;
+
+    private bool exit = false;
+    private Transform target;
+
+    float time = 0;
+    float blinktime = 0.2f;
+    float xtime = 0;
+    float waittime = 0.2f;
+
+    float r, g, b, z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkTile = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < 0.5f) // 버프 지속시간 -3초
+        {
+            checkTile.color = new Color(r, g, b, z); // 처음엔 켜져있고
+        }
+        else // 약 3초
+        {
+            if (xtime < blinktime) // 깜빡
+            {
+                checkTile.color = new Color(r, g, b, z - xtime * 2.5f); //꺼졌다가
+            }
+            else if (xtime < waittime + blinktime)
+            {
+
+            }
+            else
+            {
+                checkTile.color = new Color(r, g, b, (xtime - (waittime + blinktime)) * 2.5f);
+                //켜졌다가
+                if (xtime > waittime + blinktime * 2)
+                {
+                    xtime = 0;
+                }
+            }
+            xtime += Time.deltaTime;
+        }
+        time += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Exit")
+        {
+            r = 0f;
+            g = 1f;
+            b = 1f;
+            z = 0.2f;
+            checkTile.color = new Color(r, g, b, z);
+        }
+        else if (other.tag == "Food")
+        {
+            r = 0f;
+            g = 1f;
+            b = 1f;
+            z = 0.2f;
+            checkTile.color = new Color(r, g, b, z);
+        }
+        else if (other.tag == "Battery")
+        {
+            r = 0f;
+            g = 1f;
+            b = 1f;
+            z = 0.2f;
+            checkTile.color = new Color(r, g, b, z);
+        }
+        else if (other.tag == "Soda")
+        {
+            r = 0f;
+            g = 1f;
+            b = 1f;
+            z = 0.2f;
+            checkTile.color = new Color(r, g, b, z);
+        }
+        else if (other.tag == "Wall")
+        {
+            r = 1f;
+            g = 1f;
+            b = 0f;
+            z = 0.2f;
+            checkTile.color = new Color(r, g, b, z);
+        }
+        else if (other.tag == "OuterWall")
+        {
+            r = 1f;
+            g = 0f;
+            b = 0f;
+            z = 0.5f;
+            checkTile.color = new Color(r, g, b, z);
+        }
+        else if (other.tag == "Enemy")
+        {
+            r = 1f;
+            g = 0f;
+            b = 0f;
+            z = 0.2f;
+            checkTile.color = new Color(r, g, b, z);
+        }
+        else if (other.tag == "Untagged")
+        {
+            r = 1f;
+            g = 1f;
+            b = 1f;
+            z = 0.2f;
+            checkTile.color = new Color(r, g, b, z);
+        }
+    }
+}
---
+++ CaveWithNoNameV2/Assets/Scripts/Player.cs
@@ -0,0 +1,210 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerBattery = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    public Text foodText;
+    public AudioClip moveSound1;
+    public AudioClip moveSound2;
+    public AudioClip eatSound1;
+    public AudioClip eatSound2;
+    public AudioClip drinkSound1;
+    public AudioClip drinkSound2;
+    public AudioClip gameOverSound;
+
+    public Slider FoodSlider;
+    public Slider BatterSlider;
+
+    private GameObject flashlight;
+    private Quaternion rotation;
+
+    private Animator animator;
+    private int food;
+    private int battery;
+    private float lightScaleX = 3.5f, lightScaleY = 7f, lightScaleZ = 7f;
+
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        flashlight = GameObject.Find("Light");
+        flashlight.SetActive(true);
+        flashlight.transform.localScale = new Vector3(lightScaleX, lightScaleY, lightScaleZ);
+
+        food = GameManager.instance.playerFoodPoints;
+        battery = GameManager.instance.playerBatteryPoints;
+
+        FoodSlider.value = food;
+        BatterSlider.value = battery;
+
+        foodText.text = "Food: " + food;
+
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.playerFoodPoints = food;
+        GameManager.instance.playerBatteryPoints = battery;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameManager.instance.playersTurn) return;
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal"); // 가로
+        vertical = (int)Input.GetAxisRaw("Vertical"); // 세로
+
+        transform.Translate(Vector3.right * Time.deltaTime * horizontal, Space.World);
+        transform.Translate(Vector3.up * Time.deltaTime * vertical, Space.World);
+        // 테스트를 위한 키보드 이동 끝
+
+
+        // 오브젝트에 따른 HP Bar 위치 이동
+        flashlight.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+
+
+        if (horizontal != 0)
+            vertical = 0;
+
+        if (horizontal != 0 || vertical != 0)
+            AttempMove<Wall>(horizontal, vertical);
+    }
+
+    protected override void AttempMove<T>(int xDir, int yDir)
+    {
+        if (xDir == 1f)
+        {
+            rotation = Quaternion.Euler(0, 0, 90);
+            flashlight.transform.rotation = rotation;
+        }
+        else if (xDir == -1f)
+        {
+            rotation = Quaternion.Euler(0, 0, -90);
+            flashlight.transform.rotation = rotation;
+        }
+        else if(yDir == 1f)
+        {
+            rotation = Quaternion.Euler(0, 0, 180);
+            flashlight.transform.rotation = rotation;
+        }
+        else if(yDir == -1f)
+        {
+            rotation = Quaternion.Euler(0, 0, 0);
+            flashlight.transform.rotation = rotation;
+        }
+
+        food--;
+        battery--;
+        lightScaleX -= 0.05f;
+        lightScaleY -= 0.05f;
+        lightScaleZ -= 0.05f;
+        flashlight.transform.localScale = new Vector3(lightScaleX, lightScaleY, lightScaleZ);
+
+        FoodSlider.value = food;
+        BatterSlider.value = battery;
+
+        foodText.text = "Food: " + food;
+
+        base.AttempMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+        if (Move(xDir, yDir, out hit))
+        {
+            SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+        }
+
+
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if (other.tag == "Food")
+        {
+            food += pointsPerFood;
+            if (food > 200)
+                food = 200;
+            FoodSlider.value = food;
+            foodText.text = "+" + pointsPerFood + " Food: " + food;
+            SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            if (food > 200)
+                food = 200;
+            FoodSlider.value = food;
+            foodText.text = "+" + pointsPerSoda + " Food: " + food;
+            SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Battery")
+        {
+            battery += pointsPerBattery;
+            if (battery > 200)
+                battery = 200;
+            BatterSlider.value = battery;
+            lightScaleX += 0.15f;
+            lightScaleY += 0.15f;
+            lightScaleZ += 0.15f;
+            flashlight.transform.localScale = new Vector3(lightScaleX, lightScaleY, lightScaleZ);
+            //foodText.text = "+" + pointsPerSoda + " Food: " + food;
+            SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
+    }
+
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food -= loss;
+       // battery -= loss;
+
+        FoodSlider.value -= loss;
+
+        foodText.text = "-" + loss + " Food: " + food;
+        CheckIfGameOver();
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0 || battery <= 0)
+        {
+            SoundManager.instance.PlaySingle(gameOverSound);
+            SoundManager.instance.musicSource.Stop();
+            GameManager.instance.GameOver();
+        }
+    }
+}
